4c705ed5b585fcd5ab5fd08409a83b49abf4181e
SayleeSabde <66075247+SayleeSabde@users.noreply.github.com>
2020-07-06 02:21:49 -0700

    Update Plus One.cpp

--- Plus One.cpp
+++ Plus One.cpp
@@ -8,35 +8,17 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
        int i = digits.size()-1;
-       int count = 0;
-       
-       while(i >= 0){
-           if(digits[i] == 9) {
-                count++;
-                i--;
-           }
-           else 
-               break;
+       while(i >= 0 && digits[i] == 9){
+            digits[i] = 0;
+            i--;
        }
-       if(count == 0) {
-           digits[digits.size()-1] = digits[digits.size()-1] + 1;
-       }
-       else {
-           i++;
-           if(i == 0) {
-                digits[i] = 1;
-                i++;
-                digits.push_back(0);
-           }
-           else 
-               digits[i-1] = digits[i-1] + 1;
-                
-           while(i < digits.size()) {
-                digits[i] = 0;
-                i++;
-           }
+      if(i == -1) {
+            digits[i+1] = 1;
+            digits.push_back(0);
+      }
+      else 
+            digits[i] = digits[i] + 1;
            
-       }
         return digits;
     }
 };
